5dee28faf44109715a30e8997fb5e5d4dd1ec5c3
remi.odufuye <remi.odufuye@gmail.com>
2019-10-03 13:06:13 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,9 +3,9 @@ function forLoop(array) {
   
   for ( i=0 ; i<25 ; i++) {
     if (i = 1) {
-      "I am 1 strange loop."
+      `I am 1 strange loop`
     } else {
-      "I am ${i} strange loops."
+      `I am ${i} strange loops`
     }
     
   }
