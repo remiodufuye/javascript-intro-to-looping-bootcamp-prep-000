9aaf71043c1f0591c6a3b6f9683b211109ce49f3
remi.odufuye <remi.odufuye@gmail.com>
2019-10-03 13:12:54 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,12 +3,11 @@ function forLoop(array) {
   
   for ( i=0 ; i<25 ; i++) {
 
-      console.log (`I am ${i} strange loops`);
+      `I am ${i} strange loops`
     }
      return array ; 
   }
   
-  forLoop("teddy bear") ; 
   
 
     
