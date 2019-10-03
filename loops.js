d65337ff5547a65aab3a73f754807794aaa19e39
remi.odufuye <remi.odufuye@gmail.com>
2019-10-03 13:26:06 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -5,9 +5,9 @@ function forLoop(array) {
       if ( i===1) {
        array.push("I am 1 strange loop.") ; 
       }
-      else {
+      else 
           array.push(`I am ${i} strange loops`);
-      }
+      
     }
      return array ; 
   }
